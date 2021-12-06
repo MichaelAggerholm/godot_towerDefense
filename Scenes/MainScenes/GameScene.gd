9d6a3cce9f4d4ebd58033bed8c9fca40041c32f0
michael <92aggerholm@gmail.com>
2021-12-06 07:22:27 +0100

    updated towers and menu

---
+++ Scenes/MainScenes/GameScene.gd
@@ -0,0 +1,29 @@
+extends Node2D
+
+var map_node
+
+var build_mode = false
+var build_valid = false
+var build_location
+var build_type
+
+func _ready():
+	pass
+
+func process(delta):
+	pass
+
+func _unhandled_input(event):
+	pass
+
+func initiate_build_mode(twoer_type):
+	pass
+
+func update_tower_preview():
+	pass
+
+func cancel_build_mode():
+	pass
+
+func verify_and_build():
+	pass
